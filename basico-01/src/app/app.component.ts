64d46fed3fd6b9f68eac663d872a54117168e954
alessandro lorenz <alessandro.lorenz@compasso.com.br>
2020-02-04 16:14:11 -0300

    Two way binding and services 01

--- basico-01/src/app/app.component.ts
+++ basico-01/src/app/app.component.ts
@@ -1,62 +1,23 @@
 import { Component } from '@angular/core';
+import { UsuariosService } from './usuarios.services';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <p>data binding</p>
-  <img src="{{imageURL}}"/>
-  <p>prop binding - usa a por src da DOM entre colchetes sem parentesis na variavel</p>
-  <img [src]="imageURL"/> 
-<hr/>
-<p>string interpolation</p>
-  <button [disabled] = "buttonStatus">Botao1</button>
-  <p>class binding</p>
-  <button [class.active] = "isActive">Botao2</button>
-  <p>style binding</p>
-  <button [style.borderColor] = "isActive ? 'green' : 'blue'">Botao2</button>
-  <p>DOM style object properties-list of props</p>
-<hr/>
-  <p>event binding</p>
-  <button (click) = "save($event)">Clique e olhe o console</button> 
-  <input type="text" (keyup) = "onKeyUp($event)"/>
-  <input type="text" (keyup.enter) = "onKeyUp1()"/>
-  <input type="text" (keyup.enter) = "onKeyUp2($event)"/>
-  <p>template variable</p>
-  <input type="text" (keyup.enter) = "onKeyUp3(nome.value)" #nome />
-  
-
-  `,
-  styles: [`
-    .active{
-      background: green;
-    }
-  
-  `]
+  templateUrl: "./app.component.html",
+  styles: [``]
 })
 
 export class AppComponent {
-  title = 'hello-world';
-  title1 = 'Yeap!!';
-  imageURL = "https://avatars0.githubusercontent.com/u/38251182?s=400&u=93f470c75494e57adffc47d6eb9ce666df9e9aaf&v=4";
-  buttonStatus = true;
-  isActive = true;
-  save(e){
-    console.log(e);
+  pessoas : any = [];
+  constructor(){
+    let usuarios = new UsuariosService;
+    this.pessoas = usuarios.getUusuarios();
+
   }
 
-  onKeyUp(e) {
-    if(e.keyCode === 13){
-      console.log("enter foi pressionado");
-    }
-  }
-  onKeyUp1() {
-      console.log("enter foi pressionado");
-  }
-  onKeyUp2(e) {
-      console.log(e.target.value);
-  }
-  onKeyUp3(nome) {
-      console.log(nome);
+  coisa = {
+    nome: "Pedro",
+    idade: 30
   }
 
 
---
+++ basico-01/src/app/usuarios.services.ts
@@ -0,0 +1,22 @@
+
+export class UsuariosService {
+  getUusuarios(){
+    return [
+      {
+        "nome": "Ale",
+        "idade": 35,
+        "hobby": 'seda'
+      },
+      {
+        "nome": "Santo",
+        "idade": 35,
+        "hobby": 'seda'
+      },
+      {
+        "nome": "Pedro",
+        "idade": 35,
+        "hobby": 'algodão'
+      },
+    ]
+  }
+}

e0154a04171c8881e4c38311a3c51f1569cc955d
alessandro lorenz <alessandro.lorenz@compasso.com.br>
2020-02-04 10:59:21 -0300

    Create a module with ng serve

---
+++ basico-01/src/app/cursos/cursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cursos',
+  template: `<h3>Teste</h3>`,
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
